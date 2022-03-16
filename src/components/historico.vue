fec86eb35725a7038ef464d6ae97b0338591a719
DieniK <dieni.silva@solinftec.com>
2022-03-16 17:24:22 -0300

    sonar2

--- src/components/historico.vue
+++ src/components/historico.vue
@@ -112,9 +112,9 @@
     </button>
     <div v-if="showModal" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
       <div class="relative w-auto my-6 mx-auto max-w-sm">
-        <!--content-->
+       
         <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-          <!--header-->
+         
           <div class="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
             <h3 class="text-3xl font-semibold">
               Cancelar
@@ -125,13 +125,13 @@
               </span>
             </button>
           </div>
-          <!--body-->
+          
           <div class="relative p-6 flex-auto">
             <p class="my-4 text-blueGray-500 text-lg leading-relaxed">
               Você tem certeza? Ao clicar em salvar você irá excluir a ordem em aberto permanentemente.
             </p>
           </div>
-          <!--footer-->
+         
           <div class="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
             <button class="text-red-500 bg-transparent border border-solid border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" v-on:click="toggleModal()">
               Close
